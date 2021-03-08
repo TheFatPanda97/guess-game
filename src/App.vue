f1e1641a2a3dda67bdddd233b6447ea6699b5ce7
TheFatPanda97 <shawnhu97@gmail.com>
2021-03-07 21:07:25 -0500

    push dist

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div>
+		<h1 id="title">Welcome to Guess Game!</h1>
+		<div id="form">
+			<input v-model="guess" type="number" placeholder="Enter a number" />
+			<button @click="onGuess" class="guess-btn">Guess</button>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	data() {
+		return {
+			guess: 0,
+			secretNum: Math.round(Math.random() * 10),
+		}
+	},
+	methods: {
+		onGuess() {
+			let msg
+			if (this.guess > this.secretNum) {
+				msg = "your guess was too big!"
+			} else if (this.guess < this.secretNum) {
+				msg = "your guess was too small"
+			} else {
+				msg = "you guessed correctly, yayyyyy! The game has restarted."
+                this.secretNum = Math.round(Math.random() * 10)
+			}
+
+			alert(msg)
+		},
+	},
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+#title {
+	text-align: center;
+}
+
+#form {
+	text-align: center;
+}
+
+input {
+	margin: 0px 5px;
+}
+
+.guess-btn {
+	background-color: black;
+	color: white;
+	border: none;
+	width: 60px;
+	height: 20px;
+	border-radius: 15%;
 }
 </style>
